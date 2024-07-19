6486ab6a4c0d40d7c0f8b25bfb3ff5434a719211
Julián Medina <juliandm1995@gmail.com>
2024-07-19 13:23:54 -0500

    wip

--- src/app/(webapp)/_src/CreditCard.tsx
+++ src/app/(webapp)/_src/CreditCard.tsx
@@ -1,106 +1,109 @@
+import { Box } from '@mantine/core'
 import Card from 'react-animated-3d-card'
 
 export const CreditCard = () => {
   return (
-    <Card
-      style={{
-        background: 'linear-gradient(to right, #f0f0f0, #c1c1c1, #e4e4e4)',
-        width: '450px',
-        height: '300px',
-        cursor: 'pointer',
-      }}
-      onClick={() => console.log('Hola')}
-    >
-      <div>
-        <img
-          style={{
-            position: 'absolute',
-            left: '25px',
-            top: '25px',
-            height: '50px',
-          }}
-          src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
-          className="card-item__chip"
-          alt="credit card chip"
-        ></img>
-        <img
-          style={{
-            position: 'absolute',
-            right: '25px',
-            top: '25px',
-            height: '50px',
-          }}
-          src="https://www.tuya.com.co/sites/all/themes/tuya/assets/images/pre-home/logo_tuya.svg"
-          className="card-item__chip"
-          alt="credit card chip"
-        ></img>
-      </div>
-      <div
+    <Box style={{ transform: 'scale(0.5)' }} w={'90%'}>
+      <Card
         style={{
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          background: 'linear-gradient(to right, #f0f0f0, #c1c1c1, #e4e4e4)',
+          width: '450px',
+          height: '300px',
+          cursor: 'pointer',
         }}
+        onClick={() => console.log('Hola')}
       >
+        <div>
+          <img
+            style={{
+              position: 'absolute',
+              left: '25px',
+              top: '25px',
+              height: '50px',
+            }}
+            src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png"
+            className="card-item__chip"
+            alt="credit card chip"
+          ></img>
+          <img
+            style={{
+              position: 'absolute',
+              right: '25px',
+              top: '25px',
+              height: '50px',
+            }}
+            src="https://www.tuya.com.co/sites/all/themes/tuya/assets/images/pre-home/logo_tuya.svg"
+            className="card-item__chip"
+            alt="credit card chip"
+          ></img>
+        </div>
         <div
           style={{
-            fontSize: '30px',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          <label>1234</label>
-          <label style={{ marginLeft: '30px' }}>1234</label>
-          <label style={{ marginLeft: '30px' }}>1234</label>
-          <label style={{ marginLeft: '30px' }}>1234</label>
+          <div
+            style={{
+              fontSize: '30px',
+            }}
+          >
+            <label>1234</label>
+            <label style={{ marginLeft: '30px' }}>1234</label>
+            <label style={{ marginLeft: '30px' }}>1234</label>
+            <label style={{ marginLeft: '30px' }}>1234</label>
+          </div>
         </div>
-      </div>
-      <div>
-        <label
-          style={{
-            position: 'absolute',
-            bottom: '60px',
-            left: '25px',
-            opacity: 0.5,
-          }}
-        >
-          Card holder
-        </label>
-        <label
-          style={{
-            position: 'absolute',
-            bottom: '60px',
-            right: '25px',
-            opacity: 0.5,
-          }}
-        >
-          Expires
-        </label>
-      </div>
+        <div>
+          <label
+            style={{
+              position: 'absolute',
+              bottom: '60px',
+              left: '25px',
+              opacity: 0.5,
+            }}
+          >
+            Card holder
+          </label>
+          <label
+            style={{
+              position: 'absolute',
+              bottom: '60px',
+              right: '25px',
+              opacity: 0.5,
+            }}
+          >
+            Expires
+          </label>
+        </div>
 
-      <div>
-        <label
-          style={{
-            position: 'absolute',
-            bottom: '25px',
-            left: '25px',
-            opacity: 1,
-            fontSize: '25px',
-          }}
-        >
-          Paul Doe
-        </label>
-        <label
-          style={{
-            position: 'absolute',
-            bottom: '25px',
-            right: '25px',
-            opacity: 1,
-            fontSize: '25px',
-          }}
-        >
-          12/24
-        </label>
-      </div>
-    </Card>
+        <div>
+          <label
+            style={{
+              position: 'absolute',
+              bottom: '25px',
+              left: '25px',
+              opacity: 1,
+              fontSize: '25px',
+            }}
+          >
+            Paul Doe
+          </label>
+          <label
+            style={{
+              position: 'absolute',
+              bottom: '25px',
+              right: '25px',
+              opacity: 1,
+              fontSize: '25px',
+            }}
+          >
+            1/24
+          </label>
+        </div>
+      </Card>
+    </Box>
   )
 }
